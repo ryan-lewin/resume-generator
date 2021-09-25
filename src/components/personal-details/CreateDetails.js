ee01c75d55af6ad2252eb15a02b8294a4e81c835
Ryan Lewin <ryandevlewin@gmail.com>
2021-09-25 20:50:05 +1000

    Adds personal details section and implements state

--- src/components/personal-details/CreateDetails.js
+++ src/components/personal-details/CreateDetails.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react'
 
 export default class Details extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: '',
+            address: '',
+            number: '',
+            email: '',
+            linkedin: '',
+            github: '',
+            website: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.submitDetails = this.submitDetails.bind(this);
+    }
+
+    handleChange = event => {
+        const value = event.target.value;
+        this.setState({
+            ...this.state, [event.target.name]: value
+        })
+    }
+
+    submitDetails = event => {
+        // Will pass up state to create side
+    } 
+
     render() {
         return (
             <div className='inputContainer'>
                 <h3>Personal Information</h3>
                 <label htmlFor='name'>Your name</label>
-                <input id='name'></input>
+                <input name='name' value={this.state.name || ''} onChange={this.handleChange}></input>
 
                 <label htmlFor='Address'>Your address</label>
-                <input id='Address'></input>
+                <input name='address' value={this.state.address || ''} onChange={this.handleChange}></input>
 
                 <label htmlFor='number'>Your number</label>
-                <input type='tel' id='number'></input>
+                <input type='tel' name='number' value={this.state.number || ''} onChange={this.handleChange}></input>
 
                 <label htmlFor='email'>Your email</label>
-                <input type='email' id='email'></input>
+                <input type='email' name='email' value={this.state.email || ''} onChange={this.handleChange}></input>
 
-                <label htmlFor='linkedin'>Your linkedin</label>
-                <input id='linkedin'></input>
+                <label htmlFor='linkedin'>Your LinkedIn</label>
+                <input name='linkedin' value={this.state.linkedin || ''} onChange={this.handleChange}></input>
 
-                <label htmlFor='github'>Your website</label>
-                <input id='website'></input>
+                <label htmlFor='github'>Your Github</label>
+                <input name='github' value={this.state.github || ''} onChange={this.handleChange}></input>
+
+                <label htmlFor='github'>Your Website</label>
+                <input name='website' value={this.state.website || ''} onChange={this.handleChange}></input>
+
+                <button type='submit'>Save</button>
             </div>
         )
     }
