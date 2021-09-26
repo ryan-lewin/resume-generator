76957e1abf42fb4fa1b284f34778170f195e5c37
Ryan Lewin <ryandevlewin@gmail.com>
2021-09-26 12:13:21 +1000

    Adds functionality to experience section

--- src/components/education/create/CreateEducation.js
+++ src/components/education/create/CreateEducation.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react'
-// import uniqid from 'uniqid';
+import CreatedEducation from '../../common/CreatedDetails'
 
 import EducationSection from './CreateEducationSection'
 
 export default class Education extends Component {
+    constructor() {
+        super()
 
-    // constructor() {
-    //     super();
+        this.state = {
+            educationList: []
+        }
 
-    //     this.state = {
-    //         qualifications: {
-    //             id: uniqid(),
-    //             finishDate: Date,
-    //             institute: '',
-    //             grade: '',
-    //             highlights: [{id: uniqid(), text: ''}]
-    //         }
-    //     }
+        this.addEducation = this.addEducation.bind(this);
+    }
 
-
-    // }
+    addEducation = (education) => {
+        // console.log(this.state.educationList)
+        this.setState({educationList: this.state.educationList.concat(education)})
+        // this.setState({educationsList: this.state.educationList.concat(education)})
+        // console.log(this.state.educationList)
+    }
 
     render() {
         return (
             <div>
                 <h3>Education</h3>
-                <EducationSection />
-                <button>Add</button>
+                <EducationSection addEducation={this.addEducation} />
+                <CreatedEducation title='Added Education' items={this.state.educationList}/>
             </div>
         )
     }
 }
+
+
+// this.setState({references: this.state.references.concat(reference)})
+
+
+// this.state = {
+//     references: []
+// }
--- src/components/education/create/CreateEducationSection.js
+++ src/components/education/create/CreateEducationSection.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react'
+import uniqid from 'uniqid'
 
 export default class EducationSection extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                id: uniqid(),
+                finished: '',
+                award: '',
+                institution: '',
+                grade: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.sendEducation = this.sendEducation.bind(this);
+        this.resetForm = this.resetForm.bind(this);
+    }
+
+    handleChange = event => {
+        const value = event.target.value;
+        this.setState({
+            ...this.state, [event.target.name]: value,
+            id: uniqid(),
+        })
+    }
+
+    sendEducation = event => {
+        event.preventDefault();
+        this.props.addEducation(this.state);
+        this.resetForm();
+    }
+
+    resetForm = () => {
+        this.setState({
+            id: uniqid(),
+            finished: '',
+            award: '',
+            institution: '',
+            grade: '',
+        })
+    }
+
     render() {
         return (
-            <div className='inputContainer'>
-                <label htmlFor='finishDate'>Date finished
-                <input type='date' id='finishDate'></input>
+            <form className='inputContainer' onSubmit={this.sendEducation}>
+                <label htmlFor='finished'>Date finished
+                <input type='date' name='finished' value={this.state.finished || ''} onChange={this.handleChange}></input>
                 </label>
 
                 <label htmlFor='Award'>Award
-                <input id='Award'></input>
+                <input name='award' value={this.state.award || ''} onChange={this.handleChange}></input>
                 </label>
 
                 <label htmlFor='institution'>Institution
-                <input id='institution'></input>
+                <input name='institution' value={this.state.institution || ''} onChange={this.handleChange}></input>
                 </label>
 
                 <label htmlFor='grade'>Grade
-                <input id='grade'></input>
+                <input name='grade' value={this.state.grade || ''} onChange={this.handleChange}></input>
                 </label>
-            </div>
+
+                <button>Add</button>
+            </form>
         )
     }
 }
