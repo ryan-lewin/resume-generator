e8c8b37bdfa1f1c99d7767ba435a86fd532a263d
Ryan Lewin <ryandevlewin@gmail.com>
2021-09-26 13:59:34 +1000

    Adds delete functionality for references

--- src/components/references/create/CreateReferences.js
+++ src/components/references/create/CreateReferences.js
@@ -10,10 +10,18 @@ export default class References extends Component {
             references: []
         }
         this.addReference = this.addReference.bind(this);
+        this.removeReference = this.removeReference.bind(this);
     }
 
     addReference = (reference)  => {
         this.setState({references: this.state.references.concat(reference)})
+    }
+
+
+    removeReference = (event) => {
+        this.setState({references: this.state.references.filter(reference => {
+            return reference.id !== event.target.id;
+        })})
     }
 
     render() {
@@ -21,7 +29,7 @@ export default class References extends Component {
             <div className='section-container'>
                 <h3>References</h3>
                 <ReferenceSection addReference={this.addReference}/>
-                <CreatedReferences title='References' items={this.state.references} />
+                <CreatedReferences title='References' items={this.state.references} removeItem={this.removeReference}/>
             </div>
         )
     }
